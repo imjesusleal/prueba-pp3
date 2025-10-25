e0a5d289e48ad13dd2647f780f2806bdd4667803
Jesus Leal <jesusalbertolv@gmail.com>
2025-10-25 19:47:32 -0300

    fix: arreglo un par de cosas y borro el main que estaba dentro de api porque al pepe

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from api.auth import router as auth_routher
+from api.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
-app = FastAPI()
+app = FastAPI(
+    title="Medify API",
+    description="API de Medify aplicación para tomar citas médicas y reseñar profesionales de la salud",
+    version="1.0.0"
+)
 
-app.include_router(auth_routher)
+#health-->pruebas unitarias
+@app.get("/health", tags=["Health Check"])
+def healt_check():
+    return{"status": "ok"}
+
+@app.get("/")
+async def root():
+    return {"message": "Medify API is running", "version": "1.0.0"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routers
+app.include_router(auth_router, prefix="/api/v1")
+
